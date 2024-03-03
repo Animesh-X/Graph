e268fc89777525ece548d0221dd3aa9057e32dae
Animesh-X <animeshkumar0715@gmail.com>
2024-03-03 19:02:24 +0530

    Modify disjoint se

--- disjoint_Set.cpp
+++ disjoint_Set.cpp
@@ -1,57 +1,54 @@
 #include<iostream>
 using namespace std;
 
-class DisjointSet{
-    vector<int> rank;
-    vector<int> parent;
-    vector<int> size;
-    public:
-        DisjointSet(int n){
-            rank.resize(n+1,0);
-            parent.resize(n+1);
-            size.resize(n+1,1);
-            for (int i=0 ;i<=n; ++i) {
-                parent[i] = i;  // all elements are initially in their own set
-            }
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
+    }
 
-        int findUltimateParent(int node){
-            if(parent[node]==node){
-                return node;
-            }else{
-                return parent[node]=findUltimateParent(parent[node]);
-            }
-        }
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
 
-        void unionByRank(int u,int v){
-            int ulpU=parent[u];
-            int ulpV=parent[v];
-            if(ulpU==ulpV) return;
-            if(rank[ulpU] < rank[ulpV]){
-                parent[ulpU]=ulpV;
-            }
-            else if(rank[ulpV]<ulpU){
-                parent[ulpV]=ulpU;
-            }
-            else{
-                parent[ulpV]=ulpU;
-                rank[ulpU]++;
-            }
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
         }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
 
-        void unionBySize(int u,int v){
-            int ulpU=parent[u];
-            int ulpV=parent[v];
-            if(ulpU==ulpV) return;
-            if(size[ulpU]<size[ulpV]){
-                parent[ulpU]=ulpV;
-                size[ulpV]+=size[ulpU];
-            }
-            else{
-                parent[ulpV]=ulpU;
-                size[ulpU]+=size[ulpV];
-            }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
         }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
 };
 
 int main(){
@@ -61,14 +58,14 @@ int main(){
     ds.unionBySize(4,5);
     ds.unionBySize(6,7);
     ds.unionBySize(5,6);
-    if(ds.findUltimateParent(3)==ds.findUltimateParent(7)){
+    if(ds.findUPar(3)==ds.findUPar(7)){
         cout<<"Same Component"<<"\n";
     }
     else {
         cout<<"Not Same"<<"\n";
     }
     ds.unionByRank(3,7);
-    if(ds.findUltimateParent(3)==ds.findUltimateParent(7)){
+    if(ds.findUPar(3)==ds.findUPar(7)){
         cout<<"Same Component"<<"\n";
     }
     else {
